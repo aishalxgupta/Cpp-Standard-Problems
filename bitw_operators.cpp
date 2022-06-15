aed2275f25c43e2f7bf381189faa6ca81f194aeb
Aishal Gupta <aishalgupta3008@gmail.com>
2022-06-15 11:28:29 +0530

    Add files via upload

---
+++ bitw_operators.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a= 5&6;
+    int b= 6|9;
+
+ cout<<a<<endl; 
+// msb=sign bit by 2's complement
+ 
+ cout<<b<<endl;
+
+
+    return 0;
+}
